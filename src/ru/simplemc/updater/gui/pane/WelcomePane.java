cf9be4c452789132f90cb23763b85ccbcd904d82
Goodvise <mrmineder@gmail.com>
2021-02-22 02:53:15 +0300

    Spring update final!

--- src/ru/simplemc/updater/gui/pane/WelcomePane.java
+++ src/ru/simplemc/updater/gui/pane/WelcomePane.java
@@ -16,7 +16,7 @@ public class WelcomePane extends JPanel {
         graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         GraphicsUtils.drawBackground(graphics2D, this);
-        GraphicsUtils.drawString(graphics2D, 23, "FSElliotPro-Heavy", "ffffff", "ДОБРО ПОЖАЛОВАТЬ!", 40, 76);
+        GraphicsUtils.drawString(graphics2D, 23, "FSElliotPro-Heavy", "ffffff", "ПРИВЕТСТВУЕМ ТЕБЯ!", 40, 76);
         GraphicsUtils.drawString(graphics2D, 16, "FSElliotPro", "e7e7e6", "Поиск обновлений...", 40, 102);
     }
 }
