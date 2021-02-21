9ddd4c66aff495c7e693a187da058bb7db7d1142
Goodvise <mrmineder@gmail.com>
2021-02-21 19:38:51 +0300

    HTTPUtils refactoring

--- src/ru/simplemc/updater/utils/HTTPUtils.java
+++ src/ru/simplemc/updater/utils/HTTPUtils.java
@@ -12,9 +12,16 @@ import java.net.URL;
 
 public class HTTPUtils {
 
-    public static JSONObject get(String method, String hostname, String path, JSONObject params) throws Exception {
+    /**
+     * @param hostname - имя хоста на который отправляем запрос
+     * @param path     - путь до исполняемого файла на хосте
+     * @param params   - JSON объект с данными для отправки на хост
+     * @return - возвращает JSONObject в случае успеха
+     * @throws Exception - выбрасывает в случае проблем с соединением
+     */
+    public static JSONObject get(String hostname, String path, JSONObject params) throws Exception {
 
-        HttpURLConnection connection = openConnection(method, hostname, path);
+        HttpURLConnection connection = openConnection(hostname, path);
         connection.disconnect();
 
         DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
@@ -36,18 +43,21 @@ public class HTTPUtils {
         return (JSONObject) new JSONParser().parse(stringBuffer.toString());
     }
 
-    private static HttpURLConnection openConnection(String method, String hostName, String path) throws Exception {
+    /**
+     * @param hostname - имя хоста на который отправляем запрос
+     * @param path     - путь до исполняемого файла на хосте
+     * @return - возвращает HttpURLConnection для дальнейшей обработки
+     * @throws Exception - выбрасывает в случае проблем с соединением
+     */
+    private static HttpURLConnection openConnection(String hostname, String path) throws Exception {
 
-        HttpURLConnection connection = (HttpURLConnection) new URL(hostName + path).openConnection();
+        HttpURLConnection connection = (HttpURLConnection) new URL(hostname + path).openConnection();
         connection.setRequestProperty("User-Agent", "SimpleMinecraft.Ru Updater");
-        connection.setRequestMethod(method);
+        connection.setRequestMethod("POST");
         connection.setUseCaches(false);
-
-        if (method.equals("POST")) {
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-        }
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
 
         return connection;
     }
