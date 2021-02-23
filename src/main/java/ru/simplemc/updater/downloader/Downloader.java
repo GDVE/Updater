ecdcfd72eb4f66c9eefa3e8af02c79f184589f04
Goodvise <mrmineder@gmail.com>
2021-02-23 19:04:37 +0300

    Изменения в HTTPUtils

--- src/main/java/ru/simplemc/updater/downloader/Downloader.java
+++ src/main/java/ru/simplemc/updater/downloader/Downloader.java
@@ -1,25 +1,17 @@
 package ru.simplemc.updater.downloader;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import ru.simplemc.updater.downloader.file.DownloaderRuntimeArchiveFile;
 import ru.simplemc.updater.downloader.file.DownloaderFile;
+import ru.simplemc.updater.downloader.file.DownloaderRuntimeArchiveFile;
 import ru.simplemc.updater.gui.Frame;
 import ru.simplemc.updater.gui.ProgressBar;
 import ru.simplemc.updater.gui.pane.DownloaderPane;
 import ru.simplemc.updater.utils.HTTPUtils;
 
-import javax.net.ssl.SSLException;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
 
 public class Downloader {
 
